204cc10af623944d11f3a708ffd6ba5e26961fc2
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-18 15:19:27 +0100

    liste notlar başlandı

--- lib/model/list_model.dart
+++ lib/model/list_model.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class Product {
+  final String title, desc;
+  final Color color;
+
+  Product({required this.title, required this.desc, required this.color});
+}
+
+List<Product> products = [
+  Product(title: "Health", desc: "lorem", color: Colors.amber)
+];
