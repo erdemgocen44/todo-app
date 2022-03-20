b7545a52c323cbe1ffe61ec328a2720d5bfad910
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-20 23:27:37 +0100

    list data tamam

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:todo/Pages/components/custom_appbar.dart';
 import 'package:todo/Pages/components/list_button_box.dart';
+import 'package:todo/Pages/components/list_data.dart';
 import 'package:todo/model/list_model.dart';
 
 import 'components/searchbar_widget.dart';
@@ -18,6 +19,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: MediaQuery.of(context).size.height * 0.1,
+        decoration: BoxDecoration(
+          color: Colors.lightBlue[50],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.space_dashboard_rounded,
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[400],
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.person_outline_rounded,
+              ),
+            ),
+          ],
+        ),
+      ),
       backgroundColor: Colors.white,
       body: SafeArea(
         child: ListView(
