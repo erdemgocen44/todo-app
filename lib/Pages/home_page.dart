b37736023c03b46319629f1dc5880178764dfb50
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-20 23:17:42 +0100

    list data tamam

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -21,73 +21,15 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: ListView(
-          children: [
+          children: const [
             //appbar seaction
-            const CustomAppBarWidget(),
+            CustomAppBarWidget(),
             //searchBar seaction
-            const SearchBarWidget(),
+            SearchBarWidget(),
             //ListButton section
-            const ListButtonBox(),
+            ListButtonBox(),
             //create addList data
-
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GridView.builder(
-                shrinkWrap: true,
-                physics: ScrollPhysics(),
-                itemCount: products.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 10.0,
-                    mainAxisSpacing: 10.0),
-                itemBuilder: (context, index) => ListCard(
-                  press: () {},
-                  product: products[index],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ListCard extends StatelessWidget {
-  const ListCard({
-    Key? key,
-    required this.product,
-    required this.press,
-  }) : super(key: key);
-  final Product product;
-  final VoidCallback press;
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: press,
-      child: Container(
-        decoration: (BoxDecoration(
-          color: product.color,
-          borderRadius: BorderRadius.circular(10),
-        )),
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              product.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 19,
-              ),
-            ),
-            Text(
-              product.desc,
-              maxLines: 5,
-              style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.black54,
-                  fontWeight: FontWeight.bold),
-            ),
+            ListData(),
           ],
         ),
       ),
