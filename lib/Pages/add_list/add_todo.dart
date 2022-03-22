7287332582c3240ddbb9fc9b2791291cff5f739a
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-22 22:01:10 +0100

    addtodo appbar eklendi

--- lib/Pages/add_list/add_todo.dart
+++ lib/Pages/add_list/add_todo.dart
@@ -10,6 +10,26 @@ class Addtodo extends StatefulWidget {
 class _AddtodoState extends State<Addtodo> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        actionsIconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.dashboard_outlined,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
