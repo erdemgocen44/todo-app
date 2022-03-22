bc64e4299868d95a5aecc5bdf37451eeacdb3185
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-03-22 22:16:39 +0100

    addtodo bootomnavigatom

--- lib/Pages/add_list/add_todo.dart
+++ lib/Pages/add_list/add_todo.dart
@@ -40,6 +40,76 @@ class _AddtodoState extends State<Addtodo> {
           ),
         ],
       ),
+      bottomNavigationBar: Container(
+        height: MediaQuery.of(context).size.height * 0.13,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.blue.withOpacity(0.5),
+              spreadRadius: 2.0,
+              blurRadius: 8.0,
+            )
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[400],
+                      shape: BoxShape.circle,
+                    ),
+                    child: const Icon(
+                      Icons.check,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          //title için textformfield eklendi
+          TextFormField(
+            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+            decoration: const InputDecoration(
+              hintText: "Enter title",
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
+          TextFormField(
+            style: const TextStyle(fontSize: 16, color: Colors.black),
+            decoration: const InputDecoration(
+              hintText: "Enter descriptions",
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide.none,
+              ),
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
